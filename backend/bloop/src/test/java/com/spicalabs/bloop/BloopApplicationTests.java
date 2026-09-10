d221c71fd71ae1a2d1ab3eaeae4fe24d2f802b6b
Sidd <tahasidd33@gmail.com>
2026-09-10 19:02:41 +0530

    chore: initialize Bloop backend

---
+++ backend/bloop/src/test/java/com/spicalabs/bloop/BloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spicalabs.bloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
